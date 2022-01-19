8e12837d19f01ce1a9223c1284580224ed73af2e
Orlin Alvarado <oalvaradomorales@gmail.com>
2022-01-18 20:26:11 -0600

    Seccion 5 finalizada

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vertical_landing_page/provider/page_provider.dart';
+import 'package:vertical_landing_page/router/router.dart';
+
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PageProvider())
+      ],
+      child: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  @override
+  void initState() {
+    super.initState();
+    Flurorouter.configureRoutes();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Landing Page',
+      home: Container(),
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
+    );
+  }
+}
